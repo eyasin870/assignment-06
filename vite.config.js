e33780b51034fe9ffb03e1f3aa172b4b0977ee4c
eyasin870 <shahinulkabir10@gmail.com>
2026-06-25 21:28:42 +0600

    fix github pages path

--- vite.config.js
+++ vite.config.js
@@ -6,5 +6,5 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [
     react(),
-    tailwindcss(),],base: '/Digitools-PlatForm11/'
+    tailwindcss(),],base: '/assignment-06/'
 })
